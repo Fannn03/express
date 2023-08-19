60ab601300475ae58d75c45499e99a6cd9d26c44
Fannn03 <fann.fann771@gmail.com>
2023-08-19 21:20:24 +0700

    Create validation column

--- models/User.js
+++ models/User.js
@@ -4,11 +4,33 @@ module.exports = (sequelize, datatype) => {
         username: {
             type: datatype.STRING,
             allowNull: false,
-            unique: true
+            unique: true,
+            validate: {
+                notNull: {
+                    msg: 'Username cannot be null'
+                },
+                len: {
+                    args: [5, 12],
+                    msg: 'Username cannot lower than 5 and 12 characters'
+                },
+                notEmpty: {
+                    args: true,
+                    msg: 'Username length cannot be empty'
+                }
+            }
         },
         firstname: {
             type: datatype.STRING,
             allowNull: false,
+            validate: {
+                notNull: {
+                    msg: 'Firstname cannot be null'
+                },
+                notEmpty: {
+                    args: true,
+                    msg: 'Firstname length cannot be empty'
+                }
+            }
         },
         lastname: {
             type: datatype.STRING,

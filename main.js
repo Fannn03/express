3d167f2e3b3a9f3af361988804c0dd4dd5fdfe23
Fannn03 <fann.fann771@gmail.com>
2023-08-19 18:06:57 +0700

    using api router

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ const app = express()
 require('dotenv').config()
 const env = process.env
 
-app.get('/', (req, res) => {
-    res.send('hello world')
-})
+const ApiRouter = require('./routers/ApiRouter')
+
+app.use('/api', ApiRouter)
 
 app.listen(env.SERVER_PORT, () => {
     console.log(`Server running on port ${env.SERVER_PORT}`);

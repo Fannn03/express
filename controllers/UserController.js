246fbbec323561b4a5c50f02b441ddcc99f51774
Fannn03 <fann.fann771@gmail.com>
2023-08-19 21:20:42 +0700

    add user create

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -6,7 +6,39 @@ module.exports = {
 
         const users = await User.findAll()
 
-        return res.send(users)
+        return res.json({
+            result: 'success',
+            data: users
+        })
+
+    },
+    create: async (req, res) => {
+
+        const {username, firstname, lastname} = req.body
+        console.log(firstname);
+        try{
+            await User.create({
+                username: username,
+                firstname: firstname,
+                lastname: lastname
+            })
+        }catch(error){
+            let messages = {}
+
+            error.errors.map(error => {
+                messages[error.path] = error.message
+            })
+
+            return res.json({
+                result: 'error',
+                messages: messages
+            })
+        }
+
+        return res.json({
+            result: 'success',
+            messages: 'Data berhasil di buat!'
+        })
 
     }
 }

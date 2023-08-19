0940d2d031293b4215693465ad4cecea692d1f67
Fannn03 <fann.fann771@gmail.com>
2023-08-19 19:45:32 +0700

    using body parser

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 
 require('dotenv').config()
 const env = process.env
 
 const ApiRouter = require('./routers/ApiRouter')
-
 const Model = require('./models/Model')
 
 app.use('/api', ApiRouter)
+
+// using body parser
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
 
 app.listen(env.SERVER_PORT, () => {
     Model.syncronize

4a33a442fcc749bee30e18a197f548a01a19b54d
Fannn03 <fann.fann771@gmail.com>
2023-08-19 18:41:49 +0700

    syncronize model when server started

--- main.js
+++ main.js
@@ -6,8 +6,11 @@ const env = process.env
 
 const ApiRouter = require('./routers/ApiRouter')
 
+const Model = require('./models/Model')
+
 app.use('/api', ApiRouter)
 
 app.listen(env.SERVER_PORT, () => {
+    Model.syncronize
     console.log(`Server running on port ${env.SERVER_PORT}`);
 })

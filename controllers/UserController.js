c3d193ebff1644391532fe60f6bdc8a89425c679
Fannn03 <fann.fann771@gmail.com>
2023-08-19 18:58:30 +0700

    create sample response get users data

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,7 +1,12 @@
+const Model = require('../models/Model').model
+const User = Model.user
+
 module.exports = {
     get: async (req, res) => {
 
-        return res.send('hello world')
+        const users = await User.findAll()
+
+        return res.send(users)
 
     }
 }

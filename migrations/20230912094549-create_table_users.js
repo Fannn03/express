835c06d34d0c0e6eb860f2020de3cb37f02656cc
Fannn03 <fann.fann771@gmail.com>
2023-09-12 17:42:55 +0700

    feat(users): create migration

---
+++ migrations/20230912094549-create_table_users.js
@@ -0,0 +1,40 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      uuid: {
+        type: Sequelize.STRING(50),
+        unique: true,
+        allowNull: false
+      },
+      username: {
+        type: Sequelize.STRING(32),
+        unique: true,
+        allowNull: false
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('users')
+  }
+};
